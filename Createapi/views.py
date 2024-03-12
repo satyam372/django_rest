3e4082e42e72dff8a2e9b137aa6bc3c44425c3b2
satyam_45 <satyampokharna45@gmail.com>
2024-03-12 20:06:44 +0530

    api to update status of admin

--- Createapi/views.py
+++ Createapi/views.py
@@ -1,10 +1,20 @@
-from Createapi.serialization import SerializationClass, RaiseissueSerializer,Raiseissue_2Serializer,CombinedComplaintsSerializer
-from Createapi.models import Login, Raisecomplaint, Raisecomplaint_2
+from Createapi.serialization import SerializationClass, RaiseissueSerializer,Raiseissue_2Serializer,CombinedComplaintsSerializer,Raiseissue_3serializer,Raise_3_assign
+from Createapi.models import Login, Raisecomplaint, Raisecomplaint_2,Raisecomplaint_3
 from rest_framework.response import Response
+# the above import[Response] is class which is to create HTTP request
+# it can perform task like :- 1)Creating API HTTP response
+  # 2.return data and status code
+  # 3.  handling status code , automatic content Negotiation
 from django.contrib.auth.models import User
 from django.http import JsonResponse
+# the above import is made to import JsonResponse class 
+# which is used to specially returning json response
 from rest_framework import status
+# the above class status is imported to see the status like 200_ok_status
 from rest_framework.views import APIView
+# APIview class is the main and fundamental block of creating api endpoints
+# it provides flexible way to define behaviour of your api
+# it has features like class_based view,http method,response handling,authentication
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -13,10 +23,47 @@ from rest_framework.permissions import AllowAny
 from .models import Login  
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
-from rest_framework import generics
-from rest_framework import serializers
+# above classes are used to parse the multipart form data
+  # Multiparser can be used for parsing file. it supports file and other types also
+
+from django.utils import timezone
+from django.shortcuts import get_object_or_404
+
+# below class to update the status of administrator and datatime of assigning the complaint
+# below class uses Raisecomplaint_3 model from models.py file
+# class Raiseissue_3serializer and class Raise_3assign from serialization.py file 
 
 
+class UpdateAdministratorStatus(APIView):
+    def post(self, request):
+        # 'request' is the instance of django httpRequest class
+        # this class has info about http request, like user information
+
+
+        # 'Request' class is extended version of httpRequest class
+        # used for handling API requests 
+
+
+
+
+        # Retrieve the Raisecomplaint instance
+         # from Raisecomplaint model
+         # retrive speg
+        complaint_id = request.data.get('complaint')
+        # this
+        raisecomplaint_instance = get_object_or_404(Raisecomplaint, complaint_id=complaint_id)
+
+        # Create a new Raisecomplaint_3 instance
+        register_serializer = Raiseissue_3serializer(data=request.data)
+        if register_serializer.is_valid():
+            register_instance = register_serializer.save(complaint=raisecomplaint_instance)
+            register_instance.administrator_status = "assigned"
+            register_instance.administrator_assign_time = timezone.now()
+            register_instance.save()
+
+            return Response(register_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(register_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 #  below is the class to retrive the complaints
 class CombinedComplaintsView(APIView):
@@ -33,6 +80,9 @@ class CombinedComplaintsView(APIView):
         combined_data = raisecomplaints_serializer + raisecomplaint_2s_serializer
 
         return Response(combined_data)
+    
+
+
 
 class Registercomplaint_2(APIView):
     parser_classes = (MultiPartParser, FormParser, JSONParser)
